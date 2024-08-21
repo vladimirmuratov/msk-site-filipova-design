feca6f42270ed7d7cd72f7d91512c3592134690a
vladimir <vladimirmuratov@mail.ru>
2024-08-21 15:09:53 +0300

    add links

--- src/components/base/BaseServiceCard.jsx
+++ src/components/base/BaseServiceCard.jsx
@@ -1,44 +1,48 @@
 import {Box, Typography} from '@mui/material'
+import Link from 'next/link'
 
-export const BaseServiceCard = ({label, text, icon}) => {
+export const BaseServiceCard = ({label, text, icon, path = '#'}) => {
     return (
-        <Box
-            sx={{
-                padding: '10px',
-                border: '1px solid var(--black)',
-                borderRadius: '10px'
-            }}
-        >
+        <Link href={path} target='_blank'>
             <Box
                 sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    gap: {xs: '5px', sm: '10px', md: '15px'},
-                    marginBottom: {xs: '10px', sm: '15px'}
+                    padding: '10px',
+                    border: '1px solid var(--black)',
+                    borderRadius: '10px',
+                    height: '100%'
                 }}
             >
                 <Box
-                    component="img"
-                    src={icon}
-                    alt="icon"
                     sx={{
-                        height: '50px'
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: {xs: '5px', sm: '10px', md: '15px'},
+                        marginBottom: {xs: '10px', sm: '15px'}
                     }}
-                ></Box>
+                >
+                    <Box
+                        component="img"
+                        src={icon}
+                        alt="icon"
+                        sx={{
+                            height: '50px'
+                        }}
+                    ></Box>
+                    <Typography
+                        sx={{
+                            color: 'var(--blue)',
+                            fontWeight: 500
+                        }}
+                    >{label}</Typography>
+                </Box>
                 <Typography
-                sx={{
-                    color: 'var(--blue)',
-                    fontWeight: 500
-                }}
-                >{label}</Typography>
+                    sx={{
+                        fontSize: {xs: '10px', sm: '12px'}
+                    }}
+                >
+                    {text}
+                </Typography>
             </Box>
-            <Typography
-            sx={{
-                fontSize: {xs: '10px', sm: '12px'}
-            }}
-            >
-                {text}
-            </Typography>
-        </Box>
+        </Link>
     )
 }
