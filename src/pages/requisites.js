7570ebf0138f242c2ccb19c159d3c24ca852af84
vladimir <vladimirmuratov@mail.ru>
2024-09-06 10:33:04 +0300

    fix requisites

--- src/pages/requisites.js
+++ src/pages/requisites.js
@@ -1,5 +1,5 @@
-import {Box, Typography} from '@mui/material'
-import {phone} from '@/config'
+import {Box, Link, Typography} from '@mui/material'
+import {email, phone} from '@/config'
 
 export default function Requisites() {
     return (
@@ -13,7 +13,24 @@ export default function Requisites() {
             <Typography sx={{color: 'var(--blue)', fontWeight: 500}}>ООО "МСК"</Typography>
             <Typography><span style={{color: 'var(--blue)', fontWeight: 500}}>адрес:</span> г. Москва, Мичуринский пр-т
                 д.6</Typography>
-            <Typography><span style={{color: 'var(--blue)', fontWeight: 500}}>телефон:</span> {phone}</Typography>
+            <Link href={`tel:${phone}`}>
+                <Typography
+                    sx={{
+                        '&:hover':{
+                            textDecoration: 'underline !important'
+                        }
+                    }}
+                ><span style={{color: 'var(--blue)', fontWeight: 500}}>телефон: </span>{phone}</Typography>
+            </Link>
+            <Link href={`mailto:${email}`}>
+                <Typography
+                    sx={{
+                        '&:hover':{
+                            textDecoration: 'underline !important'
+                        }
+                    }}
+                ><span style={{color: 'var(--blue)', fontWeight: 500}}>email: </span>{email}</Typography>
+            </Link>
             <Typography><span style={{color: 'var(--blue)', fontWeight: 500}}>ИНН:</span> 9725116009</Typography>
             <Typography><span style={{color: 'var(--blue)', fontWeight: 500}}>КПП:</span> 772501001</Typography>
             <Typography><span style={{color: 'var(--blue)', fontWeight: 500}}>ОГРН:</span> 1237700147343</Typography>
