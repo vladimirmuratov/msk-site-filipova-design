04dcb8d82e68870098bcc649e65e97215ef5e9e7
vladimir <vladimirmuratov@mail.ru>
2024-12-16 17:06:26 +0300

    add webmaster

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,74 +1,62 @@
-import {Box} from '@mui/material'
-import {Footer} from '@/components/Footer'
-import {Suspense, useEffect, useState} from 'react'
-import {BaseModal} from '@/components/base/BaseModal'
-import {MessageForm} from '@/components/MessageForm'
-import DialogContentText from '@mui/material/DialogContentText'
-import {timeOut} from '@/config'
-import {Metrika} from '@/Metrika'
-import {FloatBtn} from '@/components/FloatBtn'
-import Header2 from '@/components/Header2'
-import Head from 'next/head';
+import { Box } from '@mui/material';
+import { Footer } from '@/components/Footer';
+import { Suspense, useEffect, useState } from 'react';
+import { BaseModal } from '@/components/base/BaseModal';
+import { MessageForm } from '@/components/MessageForm';
+import DialogContentText from '@mui/material/DialogContentText';
+import { timeOut } from '@/config';
+import { Metrika } from '@/Metrika';
+import { FloatBtn } from '@/components/FloatBtn';
+import Header2 from '@/components/Header2';
 
-export const Layout = ({children}) => {
-    const [isOpenForm, setOpenForm] = useState(false)
-    const [success, setSuccess] = useState(false)
-    const [failed, setFailed] = useState(false)
+export const Layout = ({ children }) => {
+    const [isOpenForm, setOpenForm] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [failed, setFailed] = useState(false);
 
     useEffect(() => {
-        let timeout
+        let timeout;
 
         if (success || failed) {
             timeout = setTimeout(() => {
-                handleCloseAnswerMessage()
-            }, timeOut)
+                handleCloseAnswerMessage();
+            }, timeOut);
         }
         return () => {
-            clearTimeout(timeout)
-        }
-    }, [success, failed])
+            clearTimeout(timeout);
+        };
+    }, [success, failed]);
 
     const handleOpenForm = () => {
-        setOpenForm(true)
-    }
+        setOpenForm(true);
+    };
 
     const handleCloseForm = () => {
-        setOpenForm(false)
-    }
+        setOpenForm(false);
+    };
 
     const handleCloseAnswerMessage = () => {
-        setSuccess(false)
-        setFailed(false)
-    }
+        setSuccess(false);
+        setFailed(false);
+    };
 
     return (
         <>
-            <Head>
-                <title>Платная госпитализация 8(499)719-81-00 МСК</title>
-                <meta name="yandex-verification" content="7bb2122b277ff1f4" />
-                <meta name="description"
-                      content="Московская Сервисная Компания, специализирующаяся на экстренной помощи при госпитализации тяжело больных пациентов. Транспортировка в медицинские учреждения Москвы, Московской области. Организация госпитализации в стационарные отделения." />
-                <meta name="keywords"
-                      content="платная госпитализация, платная палата, гинекология госпитализация, платная госпитализация в москве хирургия, платная травматология москва, госпитализация урология москва, кардиология платная госпитализация, платная госпитализация неврология, платная гастроэнтерология в москве, платный стационар терапия" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="canonical" href="https://msk-group-hospital.ru/" />
-                <link rel="icon" href="/favicon.png" />
-            </Head>
             <Box
                 className="container"
-                sx={{minHeight: '100vh'}}
+                sx={{ minHeight: '100vh' }}
             >
                 <Suspense>
-                    <Header2/>
+                    <Header2 />
                     {children}
-                    <Metrika/>
-                    <Footer onOpenForm={handleOpenForm}/>
+                    <Metrika />
+                    <Footer onOpenForm={handleOpenForm} />
                 </Suspense>
-                <FloatBtn handleOpen={handleOpenForm}/>
+                <FloatBtn handleOpen={handleOpenForm} />
             </Box>
 
             <BaseModal open={isOpenForm} handleClose={handleCloseForm} title="Связаться с нами">
-                <MessageForm handleClose={handleCloseForm} onSuccess={setSuccess} onFailed={setFailed}/>
+                <MessageForm handleClose={handleCloseForm} onSuccess={setSuccess} onFailed={setFailed} />
             </BaseModal>
 
             {success && (
@@ -89,5 +77,5 @@ export const Layout = ({children}) => {
                 </BaseModal>
             )}
         </>
-    )
-}
+    );
+};
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,15 @@ export default function Document(props) {
     return (
         <Html lang="en">
             <Head>
-                <title>МСК-Групп</title>
+                <title>Платная госпитализация 8(499)719-81-00 МСК</title>
+                <meta name="yandex-verification" content="7bb2122b277ff1f4" />
+                <meta name="description"
+                      content="Московская Сервисная Компания, специализирующаяся на экстренной помощи при госпитализации тяжело больных пациентов. Транспортировка в медицинские учреждения Москвы, Московской области. Организация госпитализации в стационарные отделения." />
+                <meta name="keywords"
+                      content="платная госпитализация, платная палата, гинекология госпитализация, платная госпитализация в москве хирургия, платная травматология москва, госпитализация урология москва, кардиология платная госпитализация, платная госпитализация неврология, платная гастроэнтерология в москве, платный стационар терапия" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="canonical" href="https://msk-group-hospital.ru/" />
+                <link rel="icon" href="/favicon.png" />
                 <DocumentHeadTags {...props} />
             </Head>
             <body>
