442e9a71cea0f749ba1e7015d26029c21fbce3f5
vladimir <vladimirmuratov@mail.ru>
2025-01-19 14:24:28 +0300

    add patient page

---
+++ src/pages/services/patient.js
@@ -0,0 +1,36 @@
+import { BaseFirstBlock } from '@/components/base/for-service-pages/BaseFirstBlock';
+import { HowToBeHospitalized } from '@/components/for-service-pages/planned-hospital/HowToBeHospitalized';
+import StableSummaryBlock from '@/components/base/for-service-pages/stable-components/StableSummaryBlock';
+import { servicePatientDescriptionText, servicesPatientInfo, stepsPatient } from '@/config';
+import Head from 'next/head';
+import { BaseBanner } from '@/components/base/BaseBanner';
+
+export default function Patient() {
+  return (
+    <>
+      <Head>
+        <title>Лечение тяжелых пациентов. Звоните: 8 (499) 719-81-00, 24/7</title>
+        <meta
+          name="description"
+          content="Нужна помощь в лечении тяжелых пациентов? Мы работаем круглосуточно! Звоните 8 (499) 719-81-00 и получите профессиональную поддержку."
+        />
+        <meta name="keywords" content="Лечение тяжелых пациентов" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <BaseBanner
+        imgPath="/images/services/patient.webp"
+        title="лечение тяжелых пациентов"
+        fontColor="var(--red)"
+        fontShadowColor="var(--white)"
+      />
+      <BaseFirstBlock
+        title="лечение тяжелобольных пациентов"
+        description={servicePatientDescriptionText}
+        services={servicesPatientInfo}
+      />
+      <HowToBeHospitalized title="госпитализация тяжелобольных" steps={stepsPatient} />
+      <StableSummaryBlock />
+    </>
+  );
+}
