13b984381c276ea6d287a55e2fd161d4461608b9
vladimir <vladimirmuratov@mail.ru>
2025-02-24 10:09:07 +0300

    update float btn

--- src/components/FloatBtn.jsx
+++ src/components/FloatBtn.jsx
@@ -1,9 +1,10 @@
-import {Fab} from '@mui/material'
-import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled'
+import { Fab, Link } from '@mui/material';
+import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled';
+import { phone } from '@/config';
 
-export const FloatBtn = ({handleOpen}) => {
+export const FloatBtn = ({ handleOpen }) => {
     return (
-        <Fab
+        /*<Fab
             onClick={handleOpen}
             color="error"
             sx={{
@@ -15,6 +16,21 @@ export const FloatBtn = ({handleOpen}) => {
             <span className="pulse-button__rings"></span>
             <span className="pulse-button__rings"></span>
             <span className="pulse-button__rings"></span>
-        </Fab>
-    )
-}
+        </Fab>*/
+
+        <Link href={`tel:${phone}`}>
+            <Fab
+                color="error"
+                sx={{
+                    position: 'fixed',
+                    bottom: { xs: '10px', sm: '30px', md: '50px' },
+                    right: { xs: '10px', sm: '30px', md: '50px' }
+                }}>
+                <PhoneEnabledIcon />
+                <span className="pulse-button__rings"></span>
+                <span className="pulse-button__rings"></span>
+                <span className="pulse-button__rings"></span>
+            </Fab>
+        </Link>
+    );
+};
