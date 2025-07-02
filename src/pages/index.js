876313a2051930f6ea56b26947d88e6223f359a3
vladimir <vladimirmuratov@mail.ru>
2025-07-02 15:14:40 +0300

    change head tag 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import PartnersBlock from '@/components/PartnersBlock';
 import DirectionsBlock from '@/components/DirectionsBlock';
 import ServiceBlock from '@/components/ServiceBlock';
 import { useRouter } from 'next/router';
-// import Head from 'next/head';
+import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
 import { Loader } from '@/components/loader/Loader';
 import { VideoBanner } from '@/components/VideoBanner';
@@ -40,6 +40,26 @@ export default function Home() {
 
     return (
         <>
+            <Head>
+                <title>Медицинская Сервисная Компания. Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7</title>
+                <meta
+                    name="description"
+                    content="Платная госпитализация в Москве. 15 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Работаем по всей территории России и СНГ. Оперативно. Финансовая прозрачность. Медицинский туризм."
+                />
+                <meta
+                    name="keywords"
+                    content="платная госпитализация, платная палата, гинекология госпитализация, платная госпитализация в москве хирургия, платная травматология москва, госпитализация урология москва, кардиология платная госпитализация, платная госпитализация неврология, платная гастроэнтерология в москве, платный стационар терапия, Медицинский туризм."
+                />
+                <meta property="og:locale" content="ru_RU" />
+                <meta property="og:title" content="Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7" />
+                <meta property="og:description" content="Платная госпитализация в Москве. 10 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Работаем по всей территории России и СНГ. Оперативно. Финансовая прозрачность. Медицинский туризм." />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://msk-group-hospital.ru/" />
+                <meta property="og:image" content="https://msk-group-hospital.ru/images/banner-3.webp" />
+                <meta property="og:image:alt" content="Медицинская Сервисная Компания. Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7" />
+                <meta property="og:image:type" content="image/webp" />
+                <meta property="og:site_name" content="Медицинская Сервисная Компания. Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7" />
+            </Head>
             {typeof isMobile === 'boolean' ? (
                 <main className={openSans.className}>
                     {isMobile ? <Banner4 /> : <VideoBanner />}
