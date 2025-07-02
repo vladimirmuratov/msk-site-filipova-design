aad3a5df73a2ab302427bf249bb324cbf3e1a723
vladimir <vladimirmuratov@mail.ru>
2025-07-02 16:00:20 +0300

    create mainpagecontent component

---
+++ src/components/content-page/MainPageContent.jsx
@@ -0,0 +1,62 @@
+import { Banner4 } from '@/components/Banner/Banner4';
+import { VideoBanner } from '@/components/VideoBanner';
+import DescriptionBlock from '@/components/DescriptionBlock';
+import { WhyWeBlock } from '@/components/WhyWeBlock';
+import { HowToBeHospitalized } from '@/components/for-service-pages/planned-hospital/HowToBeHospitalized';
+import { stepsPlanHospitalization } from '@/config/steps';
+import PartnersBlock from '@/components/PartnersBlock';
+import DirectionsBlock from '@/components/DirectionsBlock';
+import { directionsList } from '@/config/directions/directions-list';
+import ServiceBlock from '@/components/ServiceBlock';
+import { services } from '@/config/services';
+import { DiseasesBlock } from '@/components/DiseasesBlock';
+import { diseasesList } from '@/config/diseases/diseases-list';
+import { ReviewsBlock } from '@/components/ReviewsBlock';
+import { reviews } from '@/config/reviews';
+import { Loader } from '@/components/loader/Loader';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useScrollPosition } from '@/lib/useScrollPosition ';
+import { Open_Sans } from 'next/font/google';
+
+const openSans = Open_Sans({ subsets: ['latin', 'cyrillic'] });
+
+export const MainPageContent = () => {
+    const router = useRouter();
+    const [isMobile, setMobile] = useState(undefined);
+
+    useEffect(() => {
+        const os = navigator.userAgentData.platform;
+
+        if (os === 'Android' || os === 'iOS') {
+            setMobile(true);
+        } else {
+            setMobile(false);
+        }
+    }, []);
+
+    useScrollPosition('Home');
+
+    return (
+        <>
+            {typeof isMobile === 'boolean' ? (
+                <main className={openSans.className}>
+                    {isMobile ? <Banner4 /> : <VideoBanner />}
+                    <DescriptionBlock />
+                    <WhyWeBlock />
+                    <HowToBeHospitalized
+                        steps={stepsPlanHospitalization}
+                        title="как это работает"
+                    />
+                    <PartnersBlock />
+                    <DirectionsBlock directions={directionsList} router={router} />
+                    <ServiceBlock services={services} />
+                    <DiseasesBlock diseases={diseasesList} />
+                    <ReviewsBlock reviews={reviews} isMobile={isMobile} />
+                </main>
+            ) : (
+                <Loader />
+            )}
+        </>
+    );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,15 @@
-import { Open_Sans } from 'next/font/google';
-import DescriptionBlock from '@/components/DescriptionBlock';
-import PartnersBlock from '@/components/PartnersBlock';
-import DirectionsBlock from '@/components/DirectionsBlock';
-import ServiceBlock from '@/components/ServiceBlock';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
-import React, { useEffect, useState } from 'react';
-import { Loader } from '@/components/loader/Loader';
-import { VideoBanner } from '@/components/VideoBanner';
-import { ReviewsBlock } from '@/components/ReviewsBlock';
-import { reviews } from '@/config/reviews';
-import { Banner4 } from '@/components/Banner/Banner4';
-import { HowToBeHospitalized } from '@/components/for-service-pages/planned-hospital/HowToBeHospitalized';
-import { WhyWeBlock } from '@/components/WhyWeBlock';
-import { stepsPlanHospitalization } from '@/config/steps';
-import { directionsList } from '@/config/directions/directions-list';
-import { services } from '@/config/services';
-import { useScrollPosition } from '@/lib/useScrollPosition ';
-import { DiseasesBlock } from '@/components/DiseasesBlock';
-import { diseasesList } from '@/config/diseases/diseases-list';
+import { MainPageContent } from '@/components/content-page/MainPageContent';
 
-const openSans = Open_Sans({ subsets: ['latin', 'cyrillic'] });
 
 export default function Home() {
-    const router = useRouter();
-    const [isMobile, setMobile] = useState(undefined);
 
-    useEffect(() => {
-        const os = navigator.userAgentData.platform;
-
-        if (os === 'Android' || os === 'iOS') {
-            setMobile(true);
-        } else {
-            setMobile(false);
-        }
-    }, []);
-
-    useScrollPosition('Home');
 
     return (
         <>
             <Head>
-                <title>Медицинская Сервисная Компания. Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7</title>
+                <title>Медицинская Сервисная Компания. Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00,
+                    24/7</title>
                 <meta
                     name="description"
                     content="Платная госпитализация в Москве. 15 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Работаем по всей территории России и СНГ. Оперативно. Финансовая прозрачность. Медицинский туризм."
@@ -51,33 +19,20 @@ export default function Home() {
                     content="платная госпитализация, платная палата, гинекология госпитализация, платная госпитализация в москве хирургия, платная травматология москва, госпитализация урология москва, кардиология платная госпитализация, платная госпитализация неврология, платная гастроэнтерология в москве, платный стационар терапия, Медицинский туризм."
                 />
                 <meta property="og:locale" content="ru_RU" />
-                <meta property="og:title" content="Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7" />
-                <meta property="og:description" content="Платная госпитализация в Москве. 10 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Работаем по всей территории России и СНГ. Оперативно. Финансовая прозрачность. Медицинский туризм." />
+                <meta property="og:title"
+                      content="Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7" />
+                <meta property="og:description"
+                      content="Платная госпитализация в Москве. 10 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Работаем по всей территории России и СНГ. Оперативно. Финансовая прозрачность. Медицинский туризм." />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://msk-group-hospital.ru/" />
                 <meta property="og:image" content="https://msk-group-hospital.ru/images/banner-3.webp" />
-                <meta property="og:image:alt" content="Медицинская Сервисная Компания. Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7" />
+                <meta property="og:image:alt"
+                      content="Медицинская Сервисная Компания. Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7" />
                 <meta property="og:image:type" content="image/webp" />
-                <meta property="og:site_name" content="Медицинская Сервисная Компания. Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7" />
+                <meta property="og:site_name"
+                      content="Медицинская Сервисная Компания. Платная госпитализация. Медицинский туризм. 8 (499) 719-81-00, 24/7" />
             </Head>
-            {typeof isMobile === 'boolean' ? (
-                <main className={openSans.className}>
-                    {isMobile ? <Banner4 /> : <VideoBanner />}
-                    <DescriptionBlock />
-                    <WhyWeBlock />
-                    <HowToBeHospitalized
-                        steps={stepsPlanHospitalization}
-                        title="как это работает"
-                    />
-                    <PartnersBlock />
-                    <DirectionsBlock directions={directionsList} router={router} />
-                    <ServiceBlock services={services} />
-                    <DiseasesBlock diseases={diseasesList}/>
-                    <ReviewsBlock reviews={reviews} isMobile={isMobile} />
-                </main>
-            ) : (
-                <Loader />
-            )}
+            <MainPageContent />
         </>
     );
 }
