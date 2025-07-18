ef176a7a2c25aa34b42b6b9b6648518819e8f9af
vladimir <vladimirmuratov@mail.ru>
2025-07-18 13:25:39 +0300

    update title description

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,11 @@ export default function Document(props) {
                 <DocumentHeadTags {...props} />
                 <meta name="yandex-verification" content="7bb2122b277ff1f4" />
                 <meta name="google-site-verification" content="esv7d4q6D0-Wq7NhLFwyhvy2OEOwaOGF8lVqUdghggo" />
+                <title>Медицинская Сервисная Компания. Платная госпитализация. Клиники управления делами Президента РФ. 8 (499) 719-81-00, 24/7</title>
+                <meta
+                    name="description"
+                    content="Платная госпитализация в Москве. 15 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Работаем по всей территории России и СНГ. Оперативно. Финансовая прозрачность. Медицинский туризм."
+                />
             </Head>
             <body>
             <Main />
