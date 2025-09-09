f546a02f11db18a63979593ebf3d13f480e9e0a1
vladimir <vladimirmuratov@mail.ru>
2025-09-09 08:23:03 +0300

    remove title and description from _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,11 +9,6 @@ export default function Document(props) {
                 <DocumentHeadTags {...props} />
                 <meta name="yandex-verification" content="7bb2122b277ff1f4" />
                 <meta name="google-site-verification" content="esv7d4q6D0-Wq7NhLFwyhvy2OEOwaOGF8lVqUdghggo" />
-                <title>Медицинская Сервисная Компания. Платная госпитализация в Москве. Медицинские услуги. Стационар. Без ОМС — быстро, официально, с сопровождением. 8 (499) 719-81-00, круглосуточно</title>
-                <meta
-                    name="description"
-                    content="Организуем госпитализацию в клиники Москвы без полиса ОМС и направлений. Медицинские услуги. Стационар. Перевод из больниц, помощь тяжёлым пациентам, санитарный транспорт, патронаж. Всё под ключ — без очередей и бюрократии."
-                />
             </Head>
             <body>
             <Main />
