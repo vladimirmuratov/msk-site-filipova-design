1bf2f60f1551de6a55885d522ddc84af6bfbe76c
vladimir <vladimirmuratov@mail.ru>
2026-02-13 10:00:25 +0300

    update info

--- src/components/base/BaseSubLink.jsx
+++ src/components/base/BaseSubLink.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import { Box, Link } from '@mui/material';
+import { Link } from '@mui/material';
 
 export const BaseSubLink = React.memo(({
-                                          Icon = <></>,
-                                          bgColor = 'red',
-                                          color = 'white!important',
-                                          duration = 0.3,
-                                          delay = 0,
-                                          isOpen = false,
-                                          link
-                                      }) => {
-    return (
-        <Link
-          href={link}
-          target='_blank'
-            sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                borderRadius: '50%',
-                padding: '10px',
-                backgroundColor: bgColor,
-                color: color,
-                transition: `opacity ${duration}s ease-in-out ${delay}s, transform ${duration}s ease-in-out`,
-                opacity: isOpen ? 1 : 0,
-                '&:hover': {
-                    backgroundColor: bgColor,
-                    transform: 'scale(1.2)'
-                }
-            }}
-        >
+                                         Icon = <></>,
+                                         bgColor = 'red',
+                                         color = 'white!important',
+                                         duration = 0.3,
+                                         delay = 0,
+                                         isOpen = false,
+                                         link,
+                                       }) => {
+  return (
+    <Link
+      href={link}
+      target="_blank"
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '50%',
+        padding: '10px',
+        backgroundColor: bgColor,
+        color: color,
+        transition: `opacity ${duration}s ease-in-out ${delay}s, transform ${duration}s ease-in-out`,
+        opacity: isOpen ? 1 : 0,
+        '&:hover': {
+          backgroundColor: bgColor,
+          transform: 'scale(1.2)',
+        },
+      }}
+    >
 
-            <Icon />
-        </Link>
-    );
+      <Icon />
+    </Link>
+  );
 });
--- src/pages/requisites.js
+++ src/pages/requisites.js
@@ -5,81 +5,83 @@ import { address, email, phone, phone2 } from '@/config/default-values';
 
 export default function Requisites() {
     return (
-        <>
-            <Head>
-                <title>Платная госпитализация 8 (499) 719-81-00, 24/7</title>
-                <meta name="yandex-verification" content="7bb2122b277ff1f4" />
-                <meta
-                    name="description"
-                    content="Платная госпитализация в Москве. 10 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Работаем по всей территории России и СНГ. Оперативно. Финансовая прозрачность."
-                />
-                <meta
-                    name="keywords"
-                    content="платная госпитализация, платная палата, гинекология госпитализация, платная госпитализация в москве хирургия, платная травматология москва, госпитализация урология москва, кардиология платная госпитализация, платная госпитализация неврология, платная гастроэнтерология в москве, платный стационар терапия"
-                />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-            </Head>
-            <Box
-                component="section"
-                sx={{
-                    paddingY: { xs: '25px', sm: '50px' },
-                    paddingX: { xs: '10px' },
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: '5px',
-                }}
-            >
-                <Typography sx={{ color: 'var(--blue)', fontWeight: 500 }}>ООО "МСК"</Typography>
-                {/*<Typography>
+      <>
+        <Head>
+          <title>Платная госпитализация 8 (499) 719-81-00, 24/7</title>
+          <meta name="yandex-verification" content="7bb2122b277ff1f4" />
+          <meta
+            name="description"
+            content="Платная госпитализация в Москве. 10 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Работаем по всей территории России и СНГ. Оперативно. Финансовая прозрачность."
+          />
+          <meta
+            name="keywords"
+            content="платная госпитализация, платная палата, гинекология госпитализация, платная госпитализация в москве хирургия, платная травматология москва, госпитализация урология москва, кардиология платная госпитализация, платная госпитализация неврология, платная гастроэнтерология в москве, платный стационар терапия"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        </Head>
+        <Box
+          component="section"
+          sx={{
+            paddingY: { xs: '25px', sm: '50px' },
+            paddingX: { xs: '10px' },
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '5px',
+          }}
+        >
+          <Typography sx={{ color: 'var(--blue)', fontWeight: 500 }}>ООО "МСК"</Typography>
+          {/*<Typography>
                     <span style={{ color: 'var(--blue)', fontWeight: 500 }}>адрес:</span> {address}
                 </Typography>*/}
-                <Link href={`tel:${phone}`}>
-                    <Typography
-                        sx={{
-                            '&:hover': {
-                                textDecoration: 'underline !important',
-                            },
-                        }}
-                    >
-                        <span style={{ color: 'var(--blue)', fontWeight: 500 }}>телефон: </span>
-                        {phone}
-                    </Typography>
-                </Link>
-                <Link href={`tel:${phone2}`}>
-                    <Typography
-                        sx={{
-                            '&:hover': {
-                                textDecoration: 'underline !important',
-                            },
-                        }}
-                    >
-                        <span style={{ color: 'var(--blue)', fontWeight: 500, visibility: 'hidden' }}>телефон: </span>
-                        {phone2}
-                    </Typography>
-                </Link>
-                <Link href={`mailto:${email}`}>
-                    <Typography
-                        sx={{
-                            '&:hover': {
-                                textDecoration: 'underline !important',
-                            },
-                        }}
-                    >
-                        <span style={{ color: 'var(--blue)', fontWeight: 500 }}>email: </span>
-                        {email}
-                    </Typography>
-                </Link>
-                <Typography>
-                    <span style={{ color: 'var(--blue)', fontWeight: 500 }}>ИНН:</span> 9725116009
-                </Typography>
-                <Typography>
-                    <span style={{ color: 'var(--blue)', fontWeight: 500 }}>КПП:</span> 772501001
-                </Typography>
-                <Typography>
-                    <span style={{ color: 'var(--blue)', fontWeight: 500 }}>ОГРН:</span> 1237700147343
-                </Typography>
-            </Box>
-        </>
+          <Link href={`tel:${phone}`}>
+            <Typography
+              sx={{
+                '&:hover': {
+                  textDecoration: 'underline !important',
+                },
+              }}
+            >
+              <span style={{ color: 'var(--blue)', fontWeight: 500 }}>телефон: </span>
+              {phone}
+            </Typography>
+          </Link>
+          <Link href={`tel:${phone2}`}>
+            <Typography
+              sx={{
+                '&:hover': {
+                  textDecoration: 'underline !important',
+                },
+              }}
+            >
+              <span style={{ color: 'var(--blue)', fontWeight: 500, visibility: 'hidden' }}>
+                телефон:{' '}
+              </span>
+              {phone2}
+            </Typography>
+          </Link>
+          <Link href={`mailto:${email}`}>
+            <Typography
+              sx={{
+                '&:hover': {
+                  textDecoration: 'underline !important',
+                },
+              }}
+            >
+              <span style={{ color: 'var(--blue)', fontWeight: 500 }}>email: </span>
+              {email}
+            </Typography>
+          </Link>
+          <Typography>
+            <span style={{ color: 'var(--blue)', fontWeight: 500 }}>ИНН:</span> 9727115000
+          </Typography>
+          <Typography>
+            <span style={{ color: 'var(--blue)', fontWeight: 500 }}>КПП:</span> 772701001
+          </Typography>
+          <Typography>
+            <span style={{ color: 'var(--blue)', fontWeight: 500 }}>ОГРН:</span> 1257700446321
+          </Typography>
+        </Box>
+      </>
     );
 }
